797c490965a3fa7aff2793fff258f7d333d36304
zzambito <72414271+zzambito@users.noreply.github.com>
2020-11-02 08:56:48 -0500

    learned about variables and functions

---
+++ explanations.js
@@ -0,0 +1,46 @@
+// Explanations
+
+
+
+//Variables
+
+// Text
+var favColor = 'green';
+
+// List or Array
+var myFavColors = ['blue', 'red', 'green'];
+
+// Value
+var numbOfFavColors = 3;
+
+// Boolean (True/False or Yes/No Value)
+var hasGotFavColors = true;
+
+//Object
+var zacObject = {
+	firstName: 'Zac',
+	lastName: 'Zambito',
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 26,
+	isMale: true
+};
+
+
+
+//Functions
+
+var fullname = function (firstname, secondname) {
+	return firstname + ' ' + secondname;
+}
+
+var area = function (width, height) {
+	return width * height;
+}
+
+var whatIsMyFavColor = function () {
+	return 'green';
+}
+
+var doSomething = function () {
+	console.log("do Something!!!");
+}
